5a6ab215338ce042b43fe8b65fc21784ddc7a0b0
dzamfbr <dimasazzam05@gmail.com>
2026-03-12 11:20:57 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+Kesimpulan
+
+Bulan Ramadhan merupakan bulan yang sangat istimewa bagi umat Islam karena penuh dengan rahmat, ampunan, dan berbagai keutamaan. Oleh karena itu, kita harus memanfaatkan bulan Ramadhan dengan sebaik-baiknya dengan memperbanyak ibadah, memperbaiki perilaku, serta mendekatkan diri kepada Allah SWT agar mendapatkan keberkahan dan ampunan-Nya.
+
+ini kesimpulan nya
